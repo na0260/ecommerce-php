33761c78510215373bbe8e076c9a09fe71c5ccb5
na0260 <samjr.008@gmail.com>
2024-08-26 21:46:17 +0600

    index file added

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
 <?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: login.php');
+    exit();
+}
+
+require_once 'classes/Product.php';
+require_once 'classes/Cart.php';
+
+$products = Product::getAllProducts();
+$cart = isset($_SESSION['cart']) ? unserialize($_SESSION['cart']) : new Cart();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['product_id'])) {
+    $cart->addProduct($_POST['product_id'], 1);
+    $_SESSION['cart'] = serialize($cart);
+}
+
+?>
+
+<h1>Products</h1>
+<ul>
+    <?php foreach ($products as $product): ?>
+        <li>
+            <h2><?php echo $product->name; ?></h2>
+            <p><?php echo $product->description; ?></p>
+            <p>Price: $<?php echo $product->price; ?></p>
+            <form method="post">
+                <input type="hidden" name="product_id" value="<?php echo $product->id; ?>">
+                <button type="submit">Add to Cart</button>
+            </form>
+        </li>
+    <?php endforeach; ?>
+</ul>
+<a href="cart.php">View Cart</a>
+<a href="logout.php">Logout</a>
